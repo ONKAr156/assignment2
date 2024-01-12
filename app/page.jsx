ba04efbf5be9493e3524ac5ac23ce854742a4c4c
ONKAr156 <borgaonkar1998@gmail.com>
2024-01-12 13:13:31 +0530

    assignment2

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+"use client"
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const page = () => {
+  return <>
+  <div className='h-screen p-4 bg-black'>
+    <div className='h-full flex justify-center items-center flex-col gap-4 text-white'>
+      <p className='text-2xl '>Welcome to ONKAR's "Find GITHUB profile page"</p>
+      <p>To start looking for  users click the link down below</p>
+      <Link href={'/home'} className='text-blue-500 underline'>Click here</Link>
+
+    </div>
+  </div>
+  </>
+}
+
+export default page
