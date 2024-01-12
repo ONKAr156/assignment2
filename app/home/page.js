80166e9a17014078e50631287f29c4ed4c715c2e
ONKAr156 <borgaonkar1998@gmail.com>
2024-01-12 14:00:12 +0530

    assignment2

--- app/home/page.js
+++ app/home/page.js
@@ -2,9 +2,9 @@ import Home from "../../components/Home"
 import React from 'react'
 
 const page = () => {
-  return (
+  return <>
    <Home/>
-  )
+  </>
 }
 
 export default page
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,102 +1,102 @@
-"use client"
-import React, { useState } from 'react';
-import axios from 'axios';
-import Link from 'next/link';
+    "use client"
+    import React, { useState } from 'react';
+    import axios from 'axios';
+    import Link from 'next/link';
 
-const Home = () => {
-  const [username, setUsername] = useState("");
-  const [usersData, setUsersData] = useState(null);
-  const [error, setError] = useState('');
+    const Home = () => {
+    const [username, setUsername] = useState("");
+    const [usersData, setUsersData] = useState(null);
+    const [error, setError] = useState('');
 
-  const handleFetchUsersData = async (e) => {
-    e.preventDefault();
+    const handleFetchUsersData = async (e) => {
+        e.preventDefault();
 
-    try {
-      const response = await axios.get(`https://api.github.com/users/${username}`);
-      setUsersData(response.data);
-    } catch (error) {
-      if (error.response && error.response.status === 404) {
-        setError('User not found.'); // Handle the 404 error separately
-      } else {
-        setError('An error occurred. Please try again.'); 
-      }
+        try {
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        setUsersData(response.data);
+        } catch (error) {
+        if (error.response && error.response.status === 404) {
+            setError('User not found.'); // Handle the 404 error separately
+        } else {
+            setError('An error occurred. Please try again.'); 
+        }
 
-      setTimeout(() => {
-        setError(""); // Clearing the error after 3 seconds
-      }, 3000);
+        setTimeout(() => {
+            setError(""); // Clearing the error after 3 seconds
+        }, 3000);
+        }
+    };
+
+    return (
+        <div className="min-h-screen mx-auto p-4">
+        <h1 className="text-center text-2xl font-bold my-4">Check GitHub User Info</h1>
+
+        <form onSubmit={handleFetchUsersData} className="max-w-xl mx-auto my-4">
+            <input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            placeholder="Enter GitHub Username eg: ONKAr156"
+            className="border border-gray-400 text-black p-2 mb-4 w-full"
+            required
+            />
+            <div className="text-end">
+            <button
+                type="submit"
+                className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
+            >
+                Check
+            </button>
+            </div>
+        </form>
+
+        {error && <p className="text-red-600">{error}</p>}
+
+        {usersData && (
+            <div className="my-5 mx-10 h-[35rem] sm:h-[20rem] md:h-[15rem] bg-slate-950 rounded-md shadow-md overflow-hidden">
+            <div className="flex flex-col md:flex-row h-full gap-4">
+                <div className="my-2 flex items-center p-2">
+                <img
+                    className="shadow-2xl shadow-white w-full object-cover md:object-contain rounded-full md:w-48"
+                    src={usersData.avatar_url}
+                    alt={usersData.login}
+                />
+                </div>
+                <div className="p-8 h-full">
+                <div>
+                    <span>UserId: </span>
+                    <span className="text-sm text-blue-500 font-semibold">{usersData.login}</span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <span className="md:text-lg">Name:</span>
+                    <Link href={usersData.html_url} className="block mt-1 text-sm md:text-lg leading-tight font-medium text-white">
+                    {usersData.name}
+                    <span className="mx-2 font-thin text-xs">View</span>
+                    </Link>
+                </div>
+
+                <p className="mt-2 text-gray-500">Public Repositories: {usersData.public_repos}</p>
+                <p className="mt-2 text-gray-500">Public Gists: {usersData.public_gists}</p>
+                <p className="mt-2 text-gray-500">
+                    Profile Created: {new Date(usersData.created_at).toISOString().split('T')[0]}
+                </p>
+                </div>
+            </div>
+            </div>
+        )}
+
+        {usersData && (
+            <div className='text-end'>
+                <Link href={'/'} className="text-end">
+            <button  className="bg-gray-700 text-slate-50 py-2 md:py-2 px-2 md:px-4">
+                Home
+            </button>
+            </Link>
+            </div>
+        )}
+
+        </div>
+    );
     }
-  };
 
-  return (
-    <div className="min-h-screen mx-auto p-4">
-      <h1 className="text-center text-2xl font-bold my-4">Check GitHub User Info</h1>
-
-      <form onSubmit={handleFetchUsersData} className="max-w-xl mx-auto my-4">
-        <input
-          type="text"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-          placeholder="Enter GitHub Username eg: ONKAr156"
-          className="border border-gray-400 text-black p-2 mb-4 w-full"
-          required
-        />
-        <div className="text-end">
-          <button
-            type="submit"
-            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
-          >
-            Check
-          </button>
-        </div>
-      </form>
-
-      {error && <p className="text-red-600">{error}</p>}
-
-      {usersData && (
-        <div className="my-5 mx-10 h-[35rem] sm:h-[20rem] md:h-[15rem] bg-slate-950 rounded-md shadow-md overflow-hidden">
-          <div className="flex flex-col md:flex-row h-full gap-4">
-            <div className="my-2 flex items-center p-2">
-              <img
-                className="shadow-2xl shadow-white w-full object-cover md:object-contain rounded-full md:w-48"
-                src={usersData.avatar_url}
-                alt={usersData.login}
-              />
-            </div>
-            <div className="p-8 h-full">
-              <div>
-                <span>UserId: </span>
-                <span className="text-sm text-blue-500 font-semibold">{usersData.login}</span>
-              </div>
-              <div className="flex items-center gap-2">
-                <span className="md:text-lg">Name:</span>
-                <Link href={usersData.html_url} className="block mt-1 text-sm md:text-lg leading-tight font-medium text-white">
-                  {usersData.name}
-                  <span className="mx-2 font-thin text-xs">View</span>
-                </Link>
-              </div>
-
-              <p className="mt-2 text-gray-500">Public Repositories: {usersData.public_repos}</p>
-              <p className="mt-2 text-gray-500">Public Gists: {usersData.public_gists}</p>
-              <p className="mt-2 text-gray-500">
-                Profile Created: {new Date(usersData.created_at).toISOString().split('T')[0]}
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {usersData && (
-        <div className='text-end'>
-            <Link href={'/'} className="text-end">
-          <button  className="bg-gray-700 text-slate-50 py-2 md:py-2 px-2 md:px-4">
-            Home
-          </button>
-        </Link>
-        </div>
-      )}
-
-    </div>
-  );
-}
-
-export default Home;
+    export default Home;
